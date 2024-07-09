0e72fd7e47deaf2c99824d442734af65470cc68e
Ricci.rudenko <richardrudenko555@gmail.com>
2024-07-09 23:09:55 +0200

    Change User page Add send button imto header and change style of buttons

--- assets/js/components/Header.jsx
+++ assets/js/components/Header.jsx
@@ -1,16 +1,21 @@
-import React, { useContext } from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import { useTranslation } from '../context/TranslateContext';
-import { UserContext } from '../context/UserContext';
+import React, {useContext} from 'react';
+import {Link, useLocation} from 'react-router-dom';
+import {useTranslation} from '../context/TranslateContext';
+import {UserContext} from '../context/UserContext';
 import NavigationMenu from './NavigationMenu';
 
-const Header = ({ onClick }) => {
-    const { trans } = useTranslation();
-    const { isAuthenticated, userData } = useContext(UserContext);
+const Header = ({onClick}) => {
+    const {trans} = useTranslation();
+    const {isAuthenticated, userData} = useContext(UserContext);
     const location = useLocation();
 
     const renderNavigationMenu = () => {
         const pathsWithNavMenu = ['/send', '/contacts', '/template', '/history'];
+        return pathsWithNavMenu.includes(location.pathname);
+    };
+
+    const renderSendLink = () => {
+        const pathsWithNavMenu = ['/home', '/account', '/settings'];
         return pathsWithNavMenu.includes(location.pathname);
     };
 
@@ -27,54 +32,49 @@ const Header = ({ onClick }) => {
         <header>
             <div className="navbar-container">
                 <div className="nav-container">
-                    <a className="navbar-brand" href="/">
-                        <h1>RR</h1>
+                    <a href="/">
+                        <img className='logo' src="/icons/logo.svg" alt="logo"/>
                     </a>
                     {!isAuthenticated ? (
                         <>
                             <div className="d-flex">
-                                <Link to="/login" className="button navbar-button">
+                                <Link to="/login" className="button button-navbar">
                                     {trans('lang.login')}
                                 </Link>
-                                <Link to="/register" className="button navbar-button">
+                                <Link to="/register" className="button button-navbar">
                                     {trans('lang.registration')}
                                 </Link>
                             </div>
-                            <ul className="navbar-nav d-block d-md-none">
-                                <li className="dropdown-link-mobile">
-                                    <a className="nav-link " role="button" data-bs-toggle="dropdown" aria-expanded="true">
-                                        <img src="#" alt="menu" />
-                                    </a>
-                                    <ul className="dropdown-menu dropdown-menu-mobile dropdown-menu-container">
-                                        <li className="">
-                                            <a href="#">Features</a>
-                                        </li>
-                                        <li className="">
-                                            <a href="#">Price</a>
-                                        </li>
-                                        <Link to="/register" className="btn button button-gradient btn-light">
-                                            {trans('lang.registration')}
-                                        </Link>
-                                    </ul>
-                                </li>
-                            </ul>
                         </>
                     ) : (
                         <>
+                            <div className="d-flex">
+                                {isAuthenticated && renderSendLink() &&
+                                    <Link to="/send" className="button button-navbar">
+                                        {trans('lang.send')}
+                                    </Link>
+                                }
                             {userData?.accountImage ? (
-                                <div className="avatar-wrap" id='avatar-wrap' onClick={onClick}>
-                                    <img className="account-image" src={`/uploads/photos/${userData?.accountImage}`} alt="alt" />
-                                </div>
+
+                                    <div className="avatar-wrap" id='avatar-wrap' onClick={onClick}>
+                                        <img className="account-image" src={`/uploads/photos/${userData?.accountImage}`}
+                                             alt="alt"/>
+                                    </div>
+
+
+
                             ) : (
                                 <div className="default-account-image" id='avatar-wrap' onClick={onClick}>
                                     RR
                                 </div>
                             )}
+                            </div>
                         </>
                     )}
                 </div>
+
             </div>
-            {isAuthenticated && renderNavigationMenu() && <NavigationMenu />}
+            {isAuthenticated && renderNavigationMenu() && <NavigationMenu/>}
         </header>
     );
 };
--- assets/js/pages/User/UserAccount.jsx
+++ assets/js/pages/User/UserAccount.jsx
@@ -2,8 +2,8 @@ import React, {useContext} from "react";
 import {Link, NavLink, Outlet} from "react-router-dom";
 import {useTranslation} from "../../context/TranslateContext";
 import {UserContext} from "../../context/UserContext";
-import { FaMoon } from "react-icons/fa";
-import { IoMdSunny } from "react-icons/io";
+import {FaMoon} from "react-icons/fa";
+import {IoMdSunny} from "react-icons/io";
 
 export default function UserAccount() {
     const {trans} = useTranslation()
@@ -14,62 +14,49 @@ export default function UserAccount() {
     }
 
     return (
-        <div className="account-settings-container">
-            <div className="container info-account p-4">
-                <div className="row">
-                    <div className="col-12 col-lg-6 d-flex justify-content-end">
-                        <div className="wrap-account-image">
-                            <div className="account-image"></div>
-                            <a href="#">
-                                {trans('lang.edit')}
-                            </a>
-                        </div>
-                    </div>
-
-                    <div className="col-12 col-lg-6">
-                        <div className="info-account-section">
-                            <ul>
-                                <li>
-                                    <h3>{trans('lang.name')}</h3>
-                                    {userData && userData.firstName } {userData && userData.lastName }
-                                </li>
-                                <li>
-                                    <h3>
-                                        {trans('lang.email')}
-                                    </h3>
-                                    {userData && userData.email }
-                                </li>
-                            </ul>
-                            <ul className="ul-border">
-                                <li>
-                                    <h3>{trans('lang.lang')}
-                                    </h3>
-                                    <div className="d-flex justify-content-lg-around">
-                                        <a className="nav-link m-2" href="#">DE</a>
-                                        <a className="nav-link m-2" href="#">EN</a>
-                                        <a className="nav-link m-2" href="#">UA</a>
-                                    </div>
-                                </li>
-                                <li>
-                                    <h3>Design</h3>
-                                    <div className="color-site-toggle">
-
-                                        <label className="switch">
-                                            <input type="checkbox" id="theme-toggle" onChange={handleThemeToggle}/>
-                                            <span className="slider"></span>
-                                            <span className="moon">
-                                                <FaMoon />
-                                            </span>
-                                            <span className="sun">
-                                                <IoMdSunny />
-                                            </span>
-                                        </label>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+        <div className="d-flex-center account-container d-block d-md-flex">
+            <div className="d-flex-center">
+                <div className="account-image-big-wrap d-block">
+                    {userData?.accountImage
+                        ? (<img className="account-image-big" src={'uploads/photos/' + userData.accountImage}
+                                alt="Account Image"/>)
+                        : (<div className="account-image-big-wrap "></div>)}
+                    <Link to="/settings" className="link">
+                        {trans('lang.edit')}
+                    </Link>
+                    <h1 className="mt-3 f-20 fw-400 d-flex-center">
+                        <Link to="/settings">
+                            {trans('lang.editProfile')}
+                        </Link>
+                    </h1>
                 </div>
+            </div>
+
+            <div className="d-flex-center">
+                <ul>
+                    <li className="mb-3 mt-5">
+                        {userData && userData.firstName} {userData && userData.lastName}
+                    </li>
+                    <li className="mb-5">
+                        {userData && userData.email}
+                    </li>
+                    <li className="mb-5">
+                        <div className="color-site-toggle">
+                            <label className="switch">
+                                <input type="checkbox" id="theme-toggle" onChange={handleThemeToggle}/>
+                                <span className="slider"></span>
+                                <span className="moon">
+                                        <FaMoon/>
+                                    </span>
+                                <span className="sun">
+                                        <IoMdSunny/>
+                                    </span>
+                            </label>
+                        </div>
+                    </li>
+                </ul>
+
+
             </div>
         </div>
     )
